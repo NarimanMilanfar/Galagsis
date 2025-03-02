74f974ad35116097c0e7dd6a7cb2945f0f335944
Nariman Milanfar <nariman.milanfar@yahoo.com>
2025-03-02 04:43:32 -0800

    Explosion Effect Added

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    public GameObject explosionParticle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +15,13 @@ public class Shoot : MonoBehaviour
         
     }
     private void OnCollisionEnter(Collision collision)
-    {
+    {   
         if (collision.gameObject.tag == "Enemy")
         {
+           GameObject explosion= Instantiate(explosionParticle, transform.position, transform.rotation);
             Destroy(gameObject);
             Destroy(collision.gameObject);
+            Destroy(explosion, 2);
         }
     }
 }
